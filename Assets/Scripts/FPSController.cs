b4dfafcb24b6f32742c25e05e6b0f633d2751e4d
VenturiVen <gingeroshea443@gmail.com>
2024-11-01 16:15:30 +0000

    Revert "Adding from Test to main"
    This reverts commit 71ae09c89036a5602cbce2fcfdaf381ef81323fc.

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -33,6 +33,8 @@ public class FPSController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked; // locks cursor to middle!
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
