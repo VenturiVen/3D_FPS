0337a1c26fb340414e1e55811f5aac3f4de82402
VenturiVen <gingeroshea443@gmail.com>
2024-11-06 22:28:51 +0000

    GameOverScreen

--- Assets/Scripts/Player/PlayerStats.cs
+++ Assets/Scripts/Player/PlayerStats.cs
@@ -28,6 +28,16 @@ public class PlayerStats : MonoBehaviour
     public int currentCap;
     public int magSize;
 
+    [Header("States")]
+    public bool isAlive;
+
+    [Header("Panels")]
+    [SerializeField] GameObject gameOverPanel;
+
+    public void Start()
+    {
+        gameOverPanel.SetActive(false);
+    }
 
     private void Awake()
     {
@@ -68,7 +78,7 @@ public class PlayerStats : MonoBehaviour
         if (currentHP <= 0)
         {
             currentHP = 0;
-            Debug.Log("Dead");
+            PlayerDeath();
         }
         else
         {
@@ -90,6 +100,15 @@ public class PlayerStats : MonoBehaviour
     public void IncreaseScore(int num)
     {
         score += num;
+    }
+
+    public void PlayerDeath()
+    {
+        isAlive = false;
+        UnityEngine.Cursor.visible = true;
+        UnityEngine.Cursor.lockState = CursorLockMode.Confined;
+        GameObject.Destroy(gameObject);
+        gameOverPanel.SetActive(true);
     }
 
     public void IncreaseSpeed(int num)
